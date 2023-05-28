1ce1654504e4aeb36fdfba31a3abecb748df35d6
Nicolas Ramos Bohorquez <nicolas-ramos@javeriana.edu.co>
2023-05-28 12:54:41 -0500

    modificación interfaz

--- maria-output-adapter/src/main/java/co/edu/javeriana/as/personapp/mariadb/adapter/ProfesionOutputAdapterMaria.java
+++ maria-output-adapter/src/main/java/co/edu/javeriana/as/personapp/mariadb/adapter/ProfesionOutputAdapterMaria.java
@@ -1,8 +1,36 @@
 package co.edu.javeriana.as.personapp.mariadb.adapter;
 
 
+import co.edu.javeriana.as.personapp.application.port.out.ProfesionOutputPort;
+import co.edu.javeriana.as.personapp.common.annotations.Adapter;
+import co.edu.javeriana.as.personapp.domain.Profession;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
 @Slf4j
 @Adapter("ProfesionOutputAdapterMaria")
 @Transactional
-public class ProfesionOutputAdapterMaria {
+public class ProfesionOutputAdapterMaria implements ProfesionOutputPort {
+
+    @Override
+    public Profession save(Profession profesion) {
+        return null;
+    }
+
+    @Override
+    public Boolean delete(Integer identification) {
+        return null;
+    }
+
+    @Override
+    public List<Profession> find() {
+        return null;
+    }
+
+    @Override
+    public Profession findById(Integer identification) {
+        return null;
+    }
 }
--- maria-output-adapter/src/main/java/co/edu/javeriana/as/personapp/mariadb/adapter/TelefonoOutputAdapterMaria.java
+++ maria-output-adapter/src/main/java/co/edu/javeriana/as/personapp/mariadb/adapter/TelefonoOutputAdapterMaria.java
@@ -1,8 +1,35 @@
 package co.edu.javeriana.as.personapp.mariadb.adapter;
 
 
+import co.edu.javeriana.as.personapp.application.port.out.TelefonoOutputPort;
+import co.edu.javeriana.as.personapp.common.annotations.Adapter;
+import co.edu.javeriana.as.personapp.domain.Phone;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
 @Slf4j
 @Adapter("TelefonoOutputAdapterMaria")
 @Transactional
-public class TelefonoOutputAdapterMaria {
+public class TelefonoOutputAdapterMaria implements TelefonoOutputPort {
+    @Override
+    public Phone save(Phone phone) {
+        return null;
+    }
+
+    @Override
+    public Boolean delete(Integer identification) {
+        return null;
+    }
+
+    @Override
+    public List<Phone> find() {
+        return null;
+    }
+
+    @Override
+    public Phone findById(Integer identification) {
+        return null;
+    }
 }
